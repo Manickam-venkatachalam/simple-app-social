3534ebf2a3cf882b833286ec52d9a6c6efe3a9cb
Manickam <72659782+Manickam-venkatachalam@users.noreply.github.com>
2022-03-15 12:06:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const date = require('date-format')
 const app= express ();
-const PORT = 4444|| process.env.PORT;
+const PORT =process.env.PORT || 4444;
 
 const d = new Date();
 d.getFullYear();
